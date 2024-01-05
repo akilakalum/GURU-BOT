33c1afcdf310a62ff6119a3e58b202add72c6d65
akilakalum <89973219+akilakalum@users.noreply.github.com>
2024-01-05 22:20:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94742749416', 'Akila', true],
+  ['94742749416', 'Akila', true], 
   [''] 
 ] //Number of owners
 
@@ -50,8 +50,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
-global.premium = 'true'
+global.botname = akila''
+global.premium = 'flase'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
